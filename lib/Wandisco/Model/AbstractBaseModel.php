0910d8ab661b031365d222b34df51f1ef3b08151
Matthew Marcus <matthew.marcus@wandisco.com>
2013-06-19 19:13:52 -0700

    Added logging of incoming requests

---
+++ lib/Wandisco/Model/AbstractBaseModel.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * File: AbstractBaseModel.php
+ * User: matthewmarcus
+ * Date: 6/19/13
+ * Time: 1:31 PM
+ */
+
+namespace Wandisco\Model;
+
+use Zend\Stdlib\Exception\BadMethodCallException;
+
+abstract class AbstractBaseModel {
+	public function __call($called, $args){
+		if(strpos($called, 'get') == 0 || strpos($called, 'set') == 0){
+			$context = substr($called, 0, 3);
+			$propName = lcfirst(str_replace($context, NULL, $called));
+			if(property_exists($this, $propName) || property_exists($this, '_' . $propName)){
+					$propName = (property_exists($this, $propName)) ? $propName : '_' . $propName;
+			} else {
+				throw new \Exception('Undefined method property ' . get_class($this) . '::' . $propName);
+			}
+			if($context == 'get'){
+				return $this->$propName;
+			} elseif($context == 'set') {
+				if(!isset($args[0])){
+					throw new \Exception(get_class($this) . '::' . $called . ' requires arguments, none given.');
+				}
+				$this->$propName = $args[0];
+				return $this;
+			}
+		}
+		throw new BadMethodCallException('Call to non-existent method ' . get_class($this) . '::' . $called);
+	}
+}
